45d70c0f2ee63d2ec4f121029d0af1f9b8d52ca6
MapleHuang <stan60250@gmail.com>
2015-11-06 21:26:05 +0800

    Revert "Revert "init""
    This reverts commit 3de1c71bfdafebdab7bbc67df818a50aecbbec2c.

---
+++ TryNOD32/MainForm.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PBar = New System.Windows.Forms.ProgressBar()
+        Me.Label_DataDate = New System.Windows.Forms.Label()
+        Me.DBList = New System.Windows.Forms.ListView()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button_GetData = New System.Windows.Forms.Button()
+        Me.DataDateChoose = New System.Windows.Forms.DateTimePicker()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button_CopyPW = New System.Windows.Forms.Button()
+        Me.Button_CopyID = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PBar)
+        Me.GroupBox1.Controls.Add(Me.Label_DataDate)
+        Me.GroupBox1.Controls.Add(Me.DBList)
+        Me.GroupBox1.Location = New System.Drawing.Point(4, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(380, 373)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'PBar
+        '
+        Me.PBar.Location = New System.Drawing.Point(6, 350)
+        Me.PBar.MarqueeAnimationSpeed = 50
+        Me.PBar.Name = "PBar"
+        Me.PBar.Size = New System.Drawing.Size(368, 17)
+        Me.PBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.PBar.TabIndex = 2
+        '
+        'Label_DataDate
+        '
+        Me.Label_DataDate.AutoSize = True
+        Me.Label_DataDate.Location = New System.Drawing.Point(6, 16)
+        Me.Label_DataDate.Name = "Label_DataDate"
+        Me.Label_DataDate.Size = New System.Drawing.Size(77, 12)
+        Me.Label_DataDate.TabIndex = 1
+        Me.Label_DataDate.Text = "尚未取得資料"
+        '
+        'DBList
+        '
+        Me.DBList.FullRowSelect = True
+        Me.DBList.GridLines = True
+        Me.DBList.HideSelection = False
+        Me.DBList.Location = New System.Drawing.Point(6, 36)
+        Me.DBList.Name = "DBList"
+        Me.DBList.Size = New System.Drawing.Size(368, 308)
+        Me.DBList.TabIndex = 0
+        Me.DBList.UseCompatibleStateImageBehavior = False
+        Me.DBList.View = System.Windows.Forms.View.Details
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button_GetData)
+        Me.GroupBox2.Controls.Add(Me.DataDateChoose)
+        Me.GroupBox2.Location = New System.Drawing.Point(390, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(208, 56)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "取得指定日期"
+        '
+        'Button_GetData
+        '
+        Me.Button_GetData.Location = New System.Drawing.Point(142, 21)
+        Me.Button_GetData.Name = "Button_GetData"
+        Me.Button_GetData.Size = New System.Drawing.Size(51, 21)
+        Me.Button_GetData.TabIndex = 5
+        Me.Button_GetData.Text = "取得"
+        Me.Button_GetData.UseVisualStyleBackColor = True
+        '
+        'DataDateChoose
+        '
+        Me.DataDateChoose.Location = New System.Drawing.Point(8, 21)
+        Me.DataDateChoose.Name = "DataDateChoose"
+        Me.DataDateChoose.Size = New System.Drawing.Size(118, 22)
+        Me.DataDateChoose.TabIndex = 4
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button_CopyPW)
+        Me.GroupBox3.Controls.Add(Me.Button_CopyID)
+        Me.GroupBox3.Location = New System.Drawing.Point(390, 74)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(208, 78)
+        Me.GroupBox3.TabIndex = 5
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "複製至剪貼簿 (選取左方資料)"
+        '
+        'Button_CopyPW
+        '
+        Me.Button_CopyPW.Enabled = False
+        Me.Button_CopyPW.Location = New System.Drawing.Point(8, 49)
+        Me.Button_CopyPW.Name = "Button_CopyPW"
+        Me.Button_CopyPW.Size = New System.Drawing.Size(193, 22)
+        Me.Button_CopyPW.TabIndex = 1
+        Me.Button_CopyPW.Text = "複製密碼"
+        Me.Button_CopyPW.UseVisualStyleBackColor = True
+        '
+        'Button_CopyID
+        '
+        Me.Button_CopyID.Enabled = False
+        Me.Button_CopyID.Location = New System.Drawing.Point(8, 21)
+        Me.Button_CopyID.Name = "Button_CopyID"
+        Me.Button_CopyID.Size = New System.Drawing.Size(193, 22)
+        Me.Button_CopyID.TabIndex = 0
+        Me.Button_CopyID.Text = "複製帳號"
+        Me.Button_CopyID.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(603, 380)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TryNOD32 帳號密碼取得工具(2013-07-11) - MapleHuang(stan60250@gmail.com)"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents DBList As System.Windows.Forms.ListView
+    Friend WithEvents Label_DataDate As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button_GetData As System.Windows.Forms.Button
+    Friend WithEvents DataDateChoose As System.Windows.Forms.DateTimePicker
+    Friend WithEvents PBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button_CopyPW As System.Windows.Forms.Button
+    Friend WithEvents Button_CopyID As System.Windows.Forms.Button
+
+End Class
---
+++ TryNOD32/MainForm.vb
@@ -0,0 +1,165 @@
+﻿Public Class MainForm
+
+    Protected Friend WithEvents WebBrowserRead As New WebBrowser
+
+    Private Flag_Done As Boolean = False
+
+    '等待網頁讀取完成
+    Private Function Loading(ByRef [web] As WebBrowser, Optional ByVal TimeOutSec As Integer = 10) As Boolean
+        Dim WBTimeOut As Date = Now
+        Do Until [web].ReadyState = WebBrowserReadyState.Complete Or DateDiff("s", WBTimeOut, Now) >= TimeOutSec
+            Application.DoEvents()
+        Loop
+        If DateDiff("s", WBTimeOut, Now) >= TimeOutSec Then
+            [web].Stop()
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Sub CopyTextToClipboard(ByVal Text As String, Optional ByVal Quiet As Boolean = False)
+        Clipboard.SetText(Text, TextDataFormat.Text)
+        If Not Quiet Then
+            If Clipboard.GetText = Text Then
+                MsgBox("資料已成功複製", MsgBoxStyle.Information)
+            Else
+                MsgBox("資料複製失敗", MsgBoxStyle.Information)
+            End If
+        End If
+    End Sub
+
+    Private Sub WebBrowserRead_ProgressChanged(ByVal sender As Object, ByVal e As System.Windows.Forms.WebBrowserProgressChangedEventArgs) Handles WebBrowserRead.ProgressChanged
+        If e.MaximumProgress <> 0 And e.MaximumProgress >= e.CurrentProgress And Not Flag_Done Then
+            PBar.Value = Convert.ToInt32(100 * e.CurrentProgress / e.MaximumProgress)
+        Else
+            With PBar
+                .Value = 100
+                .Visible = True
+            End With
+        End If
+    End Sub
+
+    Private Sub MainForm_Shown(sender As Object, e As System.EventArgs) Handles Me.Shown
+        GetData()
+    End Sub
+
+    Private Function HTTPGet(URL As String, Optional PageEncode As String = "UTF-8") As String
+        Dim Req As Net.WebRequest
+        Dim ReceiveStream As IO.Stream
+        Dim encode As System.Text.Encoding
+        Dim sr As IO.StreamReader
+        Dim result As Net.WebResponse
+
+        Req = Net.WebRequest.Create(URL)
+        result = Req.GetResponse()
+        ReceiveStream = result.GetResponseStream
+        encode = System.Text.Encoding.GetEncoding(PageEncode)
+        sr = New IO.StreamReader(ReceiveStream, encode)
+        Return sr.ReadToEnd() '抓網頁的原始檔
+    End Function
+
+    Private Sub GetData(Optional ByVal DataDate As String = "")
+        Dim Data() As String
+        Dim i As Short
+        Dim no As Short = 0
+        Dim Flag_ID As String = ""
+        Dim Flag_Success As Boolean
+
+        Flag_Done = False
+
+        Button_GetData.Enabled = False
+        Button_CopyID.Enabled = False
+        Button_CopyPW.Enabled = False
+        Label_DataDate.Text = "正在取得資料..."
+
+        DBList.Clear()
+        With DBList
+            .View = View.Details
+            .GridLines = True
+            .Columns.Clear()
+            .Columns.Add("#", 20, HorizontalAlignment.Left)
+            .Columns.Add("帳號", 150, HorizontalAlignment.Left)
+            .Columns.Add("密碼", 150, HorizontalAlignment.Left)
+            .Refresh()
+        End With
+
+        If DataDate = "" Then
+            WebBrowserRead.Navigate("http://www.trynod32.com/")
+        Else
+            Dim Tmp As Date
+            Tmp = DateAdd(DateInterval.Day, 1, CDate(DataDate))
+            WebBrowserRead.Navigate("http://www.trynod32.com/search?updated-max=" & Tmp.Year & "-" & Tmp.Month & "-" & Tmp.Day & "T00:00:00%2B00:00&max-results=1")
+        End If
+
+        If Loading(WebBrowserRead, 6) Then
+            Label_DataDate.Text = "載入網頁完成, 嘗試取得資料..."
+        Else
+            Label_DataDate.Text = "中斷載入網頁, 嘗試取得資料..."
+        End If
+
+
+        '擷取Web網頁內的Html項目
+        For Each [HtmlElement] As HtmlElement In WebBrowserRead.Document.All
+
+            '擷取DIV-Body
+            If [HtmlElement].TagName = "DIV" And [HtmlElement].GetAttribute("ID") Like "post-body-*" Then
+                Data = Split(Replace(Trim([HtmlElement].OuterText), Chr(10), ""), Chr(13))
+                For i = LBound(Data) To UBound(Data)
+                    If Data(i) Like "Username:*" Then
+                        Flag_ID = Replace(Data(i), "Username:", "")
+                        Flag_Success = True
+                        Flag_Done = True
+                    ElseIf Data(i) Like "Password:*" Then
+                        If Flag_ID <> "" Then
+                            Dim AccountItem As ListViewItem
+                            no += 1
+                            AccountItem = DBList.Items.Add(no)
+                            AccountItem.SubItems.Add(Trim(Flag_ID))
+                            AccountItem.SubItems.Add(Trim(Replace(Data(i), "Password:", "")))
+                            Flag_ID = ""
+                        End If
+                    End If
+                Next
+            End If
+
+            '擷取資料日期
+            If [HtmlElement].TagName = "H2" And [HtmlElement].OuterText Like "*年*月*日星期*" Then
+                Label_DataDate.Text = "取得資料完成 (時間: " & [HtmlElement].OuterText & " )"
+                DataDateChoose.Value = CDate(Mid([HtmlElement].OuterText, 1, InStr([HtmlElement].OuterText, "星期") - 1))
+            End If
+        Next
+
+        If Not Flag_Success Then
+            Label_DataDate.Text = "無法取得資料"
+            PBar.Value = 0
+        ElseIf (Not Label_DataDate.Text Like "取得資料完成*") And Flag_Success And Flag_Done Then
+            Label_DataDate.Text = "取得資料完成"
+            PBar.Value = 100
+        End If
+
+        Button_GetData.Enabled = True
+    End Sub
+
+    Private Sub MainForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        WebBrowserRead.ScriptErrorsSuppressed = True
+        Me.MaximizeBox = False
+    End Sub
+
+    Private Sub Button_GetData_Click(sender As System.Object, e As System.EventArgs) Handles Button_GetData.Click
+        GetData(DataDateChoose.Value.ToString)
+    End Sub
+
+    Private Sub DBList_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles DBList.SelectedIndexChanged
+        Button_CopyID.Enabled = True
+        Button_CopyPW.Enabled = True
+    End Sub
+
+    Private Sub Button_CopyID_Click(sender As System.Object, e As System.EventArgs) Handles Button_CopyID.Click
+        CopyTextToClipboard(DBList.FocusedItem.SubItems(1).Text)
+    End Sub
+
+    Private Sub Button_CopyPW_Click(sender As System.Object, e As System.EventArgs) Handles Button_CopyPW.Click
+        CopyTextToClipboard(DBList.FocusedItem.SubItems(2).Text)
+    End Sub
+End Class
